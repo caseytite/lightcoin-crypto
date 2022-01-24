bb24ef2aed7d95651065262baf09e88fdcc569c8
Casey <caseyjtite@gmail.com>
2022-01-23 21:43:10 -0800

    finish lightcoin

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    // this.balance = 0;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let balance = 0;
+    for (let movement of this.transactions) {
+      balance += movement.value;
+    }
+    return balance;
   }
 
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed) {
+      console.log("Ya broke");
+      return false;
+    } else {
+      this.time = new Date();
+      this.account.addTransactions(this);
+      return true;
+    }
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    this.account.balance - this.amount >= 0 ? true : false;
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const mikkel = new Account("Michael");
+console.log("Mikkel");
+t1 = new Withdrawal(10, mikkel);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+// console.log("Transaction 1:", t1);
+t2 = new Withdrawal(20, mikkel);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log("Transaction 2:", t2);
+t3 = new Deposit(63, mikkel);
+t3.commit();
+// console.log("Transaction 3:", t3);
+console.log(mikkel);
+console.log(mikkel.balance);
+//------
+console.log("---------");
 
-console.log('Balance:', balance);
+const adam = new Account("Jonas");
+console.log("Adam");
+t4 = new Withdrawal(50, adam);
+t4.commit();
+// console.log("Transaction 1:", t4);
+t5 = new Withdrawal(70, adam);
+t5.commit();
+// console.log("Transaction 2:", t5);
+t6 = new Deposit(186, adam);
+t6.commit();
+// console.log("Transaction 3:", t6);
+console.log(adam);
+console.log(adam.balance);
